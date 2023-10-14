5fef48fb2d495bd64852a15672c856819ed1687e
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-14 13:34:41 +0200

    import scss file w styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import styles from "./styles/_home.module.scss";
+
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <div>
+        <h1 className={styles.header}>Hello</h1>
+        <h2>Hello world</h2>
+      </div>
     </main>
   );
 }
