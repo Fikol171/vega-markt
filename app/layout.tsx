c96f221b3de7f78889b8c6143a33756cf16ba686
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-14 13:33:54 +0200

    changed fonts to Roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import styles from "./global.module.scss";
-const inter = Inter({ subsets: ["latin"] });
+import { Roboto } from "next/font/google";
+import "./styles/global.css";
+import "normalize.css";
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700", "900"] });
 
 export const metadata: Metadata = {
   title: "Vega markt",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
