5ae7cdf6835b42309b6c81d5a20f1748bc968aa7
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-14 16:01:50 +0200

    create smartphones page

---
+++ app/pages/smartphones/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./smartpones.module.scss";
+
+const Smartphones = () => {
+  return (
+    <div>
+      <h1>Smartphones</h1>
+    </div>
+  );
+};
+
+export default Smartphones;
