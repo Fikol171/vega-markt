dad739910ac1d10e3cdca4981ddee3a4eef79b5d
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-18 10:48:16 +0200

    add mobile navigation interaction

--- app/componets/navigation/index.tsx
+++ app/componets/navigation/index.tsx
@@ -1,66 +1,127 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./_navigation.module.scss";
+import { useState } from "react";
 
 const Navigation = () => {
-  return (
-    <nav className={styles.nav}>
-      <Link href="/">
-        <Image
-          className={styles["nav__logo"]}
-          src="/mainLogo.png"
-          alt="vegaIcon"
-          width="120"
-          height="90"
-        />
-      </Link>
-      <div>
-        <label>
-          <input
-            className={styles.navInput}
-            type="text"
-            placeholder="What are You seraching?"
-          ></input>{" "}
-        </label>
-      </div>
-      <ul className={styles["nav__ul"]}>
-        <li className={styles["nav__ul--li"]}>
-          <Link className={styles["nav__link"]} href="/">
-            Home
-          </Link>
-        </li>
-        <li className={styles["nav__ul--li"]}>
-          <Link className={styles["nav__link"]} href="../../pages/smartphones">
-            Smartphones
-          </Link>
-        </li>
-        <li className={styles["nav__ul--li"]}>
-          <Link className={styles["nav__link"]} href="../../pages/laptops">
-            Laptops
-          </Link>
-        </li>
+  const [close, setClose] = useState(false);
 
-        <li className={styles["nav__ul--li"]}>
-          <Link className={styles["nav__link"]} href="../../pages/groceries">
-            Groceries
-          </Link>
-        </li>
-        <li className={styles["nav__ul--li"]}>
-          <Link className={styles["nav__link"]} href="../../pages/fasion">
-            Fasion
-          </Link>
-        </li>
-        <li className={styles["nav__ul--li"]}>
-          <Link className={styles["nav__link"]} href="../../pages/health">
-            Health
-          </Link>
-        </li>
-      </ul>
-      <button className={styles.navLoginButton}>Login</button>
-      <div className={styles.buttonContainer}>
-        <button className={styles.cartButton}>🛒</button>
-      </div>
-    </nav>
+  const toggleNavigation = () => {
+    setClose(!close);
+  };
+  return (
+    <>
+      <nav className={styles.nav}>
+        <Link href="/">
+          <Image
+            className={styles["nav__logo"]}
+            src="/mainLogo.png"
+            alt="vegaIcon"
+            width="120"
+            height="90"
+          />
+        </Link>
+        <div>
+          <button
+            className={styles.navButton}
+            onClick={() => {
+              toggleNavigation();
+            }}
+          >
+            <Image
+              src={close ? "/icon-close.svg" : "/icon-hamburger.svg"}
+              alt="button"
+              width="18"
+              height="15"
+              style={{ color: "black" }}
+            />
+          </button>
+          <label>
+            <input
+              className={styles.navInput}
+              type="text"
+              placeholder="What are You seraching?"
+            ></input>{" "}
+          </label>
+        </div>
+        <ul className={close ? styles["nav__ul"] : styles["nav__mobile"]}>
+          <li className={styles["nav__ul--li"]}>
+            <Link
+              className={styles["nav__link"]}
+              onClick={() => {
+                setClose(!close);
+              }}
+              href="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li className={styles["nav__ul--li"]}>
+            <Link
+              className={styles["nav__link"]}
+              onClick={() => {
+                setClose(!close);
+              }}
+              href="../../pages/smartphones"
+            >
+              Smartphones
+            </Link>
+          </li>
+          <li className={styles["nav__ul--li"]}>
+            <Link
+              className={styles["nav__link"]}
+              onClick={() => {
+                setClose(!close);
+              }}
+              href="../../pages/laptops"
+            >
+              Laptops
+            </Link>
+          </li>
+
+          <li className={styles["nav__ul--li"]}>
+            <Link
+              className={styles["nav__link"]}
+              onClick={() => {
+                setClose(!close);
+              }}
+              href="../../pages/groceries"
+            >
+              Groceries
+            </Link>
+          </li>
+          <li className={styles["nav__ul--li"]}>
+            <Link
+              className={styles["nav__link"]}
+              onClick={() => {
+                setClose(!close);
+              }}
+              href="../../pages/fasion"
+            >
+              Fasion
+            </Link>
+          </li>
+          <li className={styles["nav__ul--li"]}>
+            <Link
+              className={styles["nav__link"]}
+              onClick={() => {
+                setClose(!close);
+              }}
+              href="../../pages/health"
+            >
+              Health
+            </Link>
+          </li>
+        </ul>
+        <div className="navButtons">
+          <button className={styles.navLoginButton}>Login</button>
+          <div className={styles.buttonContainer}>
+            <button className={styles.cartButton}>🛒</button>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
