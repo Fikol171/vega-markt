c7e88e9d73c909c0f749f8a5d2ba8adf1775a450
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-22 16:49:45 +0200

    fetching data for main page (temporary)

---
+++ getMainPage.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const getProducts = async (props) => {
+  try {
+    const response = await axios.get(`https://dummyjson.com/products`);
+    const products = response.data;
+    return products;
+  } catch (error) {
+    return new Error(error).statusText;
+  }
+};
+export default getProducts;
