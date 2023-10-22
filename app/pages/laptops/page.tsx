ef622ca9b3f4753e16d5f2b91a0d268d78458441
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-22 16:55:39 +0200

    pages concept

--- app/pages/laptops/page.tsx
+++ app/pages/laptops/page.tsx
@@ -1,9 +1,78 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./_laptops.module.scss";
+import { usePathname } from "next/navigation";
+import getProducts from "@/getData";
+import Image from "next/image";
+import Data from "@/app/componets/interfaces";
 
 const Laptops = () => {
+  const [data, setData] = useState<Data | null>(null);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const productsData = await getProducts(pathname.slice(7));
+        setData(productsData);
+      } catch (error) {
+        console.error("Wystąpił błąd podczas pobierania danych:", error);
+      }
+    };
+
+    fetchData();
+  }, [pathname]);
+
   return (
     <div>
-      <h1>Laptops</h1>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          listStyleType: "none",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {data && data.products ? (
+          data.products.map((product) => (
+            <li
+              key={product.id}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                maxWidth: "80vw",
+                flexWrap: "wrap",
+              }}
+            >
+              <h1>{product.title}</h1>
+              <h2>{product.description}</h2>
+
+              <div
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(3, 1fr)",
+                  paddingBottom: "25px",
+                }}
+              >
+                {product.images.map((image) => (
+                  <p key={product.title}>
+                    <Image
+                      src={image}
+                      alt={product.title}
+                      width="300"
+                      height="350"
+                      style={{ margin: "30px" }}
+                    />
+                  </p>
+                ))}
+              </div>
+            </li>
+          ))
+        ) : (
+          <li>Loading...</li>
+        )}
+      </ul>
     </div>
   );
 };
