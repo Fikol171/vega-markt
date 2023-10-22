ceeb1429f31474ed78eed3ed2583f97c05188b9d
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-22 16:50:15 +0200

    changed base path and add images domain

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    domains: ["i.dummyjson.com"],
+  },
+  basePath: "/Vega",
+  publicRuntimeConfig: {
+    basePath: "/Vega",
+  },
+  serverRuntimeConfig: {
+    basePath: "/Vega",
+  },
 };
 
 module.exports = nextConfig;
