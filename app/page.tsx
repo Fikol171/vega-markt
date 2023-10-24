47e56dee8c636af9963ca4df8139644a21f57a10
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-24 12:38:33 +0200

    temporary leyout

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import Data from "./componets/interfaces";
+import Link from "next/link";
 
 export default function Home() {
   const [data, setData] = useState<Data | null>(null);
@@ -45,9 +46,66 @@ export default function Home() {
                 flexWrap: "wrap",
               }}
             >
-              <h1>{product.title}</h1>
-              <h2>{product.description}</h2>
-
+              <h1
+                style={{
+                  color: "#2c3531",
+                  margin: "10px 25px ",
+                  fontWeight: "900",
+                  fontSize: "38px",
+                }}
+              >
+                {product.title}
+              </h1>
+              <h2 style={{ color: "#2c3531", margin: "10px 25px " }}>
+                {product.description}
+              </h2>
+              <p
+                style={{
+                  color: "#d9b08c",
+                  lineHeight: "1.4",
+                  fontWeight: "bold",
+                  fontSize: "26px",
+                  margin: "10px 25px ",
+                }}
+              >
+                Price: {product.price} -euro
+              </p>{" "}
+              <p
+                style={{
+                  display: "flex",
+                  maxWidth: "1400px",
+                  padding: "20px",
+                  backgroundColor: "#2c3531",
+                  margin: "10px 25px ",
+                  borderRadius: "15px",
+                  color: "#d9b08c",
+                  lineHeight: "1.4",
+                  fontWeight: "bold",
+                  fontSize: "20px",
+                }}
+              >
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+                deserunt ut cumque cum autem rerum dolor debitis placeat commodi
+                ducimus ea velit perspiciatis, corporis molestias dolore.
+                Repellendus ipsa est esse, provident, ea autem recusandae et
+                suscipit nesciunt cupiditate non explicabo placeat iste maiores
+                quos eius dicta quam error adipisci, distinctio quidem
+                consectetur! Voluptatibus nisi ab doloribus debitis. Adipisci,
+                at optio! Possimus, rerum sit earum iure animi sequi quo odio
+                sed ipsam vitae deserunt temporibus optio doloremque voluptate
+                expedita ipsum culpa quibusdam delectus. Earum nisi animi, at
+                consequatur porro a quos, qui veniam velit voluptatibus
+                accusamus harum ad nulla nesciunt repellendus placeat, similique
+                corporis fugiat possimus necessitatibus voluptas adipisci
+                inventore quae? Eligendi doloribus quod nisi neque officiis ipsa
+                reiciendis, sit consequatur? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Quis id quaerat ea quidem! Quam
+                minima reprehenderit dolores. Sed doloremque adipisci hic
+                perspiciatis aspernatur, consequuntur officiis, explicabo
+                eveniet ipsa veniam aperiam necessitatibus reiciendis nemo
+                laborum quo porro sequi dicta. Cum, enim!
+              </p>
               <div
                 style={{
                   display: "grid",
@@ -56,14 +114,16 @@ export default function Home() {
                 }}
               >
                 {product.images.map((image) => (
-                  <p key={product.title}>
-                    <Image
-                      src={image}
-                      alt={product.title}
-                      width="300"
-                      height="350"
-                      style={{ margin: "30px" }}
-                    />
+                  <p key={product.id}>
+                    <Link href={`./pages/${product.category}`}>
+                      <Image
+                        src={image}
+                        alt={product.title}
+                        width="430"
+                        height="390"
+                        style={{ margin: "30px" }}
+                      />
+                    </Link>
                   </p>
                 ))}
               </div>
